74fd8cf6e4422d44d542e5e335828b2f77674819
cuadratico <69270131+cuadratico@users.noreply.github.com>
2023-12-01 08:56:23 +0100

    Update multiplos.sh

--- multiplos.sh
+++ multiplos.sh
@@ -1,2 +1,13 @@
 #!/bin/bash 
-echo "en espera"
+echo "Ingresa el número para encontrar sus múltiplos:"
+read numero
+
+echo "Ingresa la cantidad de múltiplos que deseas encontrar:"
+read cantidad
+
+echo "Los primeros $cantidad múltiplos de $numero son:"
+
+for ((i=1; i<=$cantidad; i++)); do
+    multiplo=$((numero * i))
+    echo "Múltiplo $i: $multiplo"
+done
