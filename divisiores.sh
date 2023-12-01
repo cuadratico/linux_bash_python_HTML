9ef92b3335799c66d9163368a951f227417a9b4f
cuadratico <69270131+cuadratico@users.noreply.github.com>
2023-12-01 08:59:08 +0100

    Update divisiores.sh

--- divisiores.sh
+++ divisiores.sh
@@ -1,2 +1,11 @@
 #!/bin/bash 
-echo "en espera"
+echo "Ingresa el número para encontrar sus divisores:"
+read numero
+
+echo "Los divisores de $numero son:"
+
+for ((i=1; i<=$numero; i++)); do
+    if [ $((numero % i)) -eq 0 ]; then
+        echo "Divisor: $i"
+    fi
+done
